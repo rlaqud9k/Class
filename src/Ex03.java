20e1ec8e9ceb3bd4a888859810ab7664308bf53f
JSLHRD <JSLHRD@JSL506-01-PC>
2018-09-20 09:51:09 +0900

    추가

---
+++ src/Ex03.java
@@ -0,0 +1,10 @@
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		String s=System.getenv("OS");//환경변수 읽어올때 사용함 환경변수는 제어판-시스템-시스템속성-고급 -환경변수에서 설정가능
+		System.out.println(s);
+
+	}
+
+}
