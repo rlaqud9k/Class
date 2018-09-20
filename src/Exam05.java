747f706bb7ffe770106f5a457b9758ef6d3b009b
JSLHRD <JSLHRD@JSL506-01-PC>
2018-09-20 09:56:19 +0900

    추가

--- src/Exam05.java
+++ src/Exam05.java
@@ -1,9 +1,12 @@
+import java.util.Properties;
 
 public class Exam05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Properties pro=System.getProperties();
+		//pro.list(System.out);
+		String osname = System.getProperty("os.name");//개발환경 구축할때 이걸 불러와서 사용함
+		System.out.println(osname);
 	}
 
 }
